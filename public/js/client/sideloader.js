4a50e6d6cca8a1281a960933f6a39bdbcaa12036
Yukimi Kazari <dev@9854.moe>
2022-01-30 20:11:09 -0500

    Added download all items function (client side)

--- public/js/client/sideloader.js
+++ public/js/client/sideloader.js
@@ -646,7 +646,7 @@ function downloadAllItems() {
                         console.log(`Saving ${downloadURLs[i]}`)
                         const url = window.URL
                             .createObjectURL(new Blob([response.data]));
-                        const link = document.createElement('temp-download');
+                        const link = document.createElement('a');
                         link.href = url;
                         link.setAttribute('download', downloadURLs[i].split('/').pop());
                         document.body.appendChild(link);
