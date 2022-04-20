5775153cdaa44b0e5da38eeb35be34f8d256b070
Yukimi Kazari <dev@9854.moe>
2022-04-20 12:04:58 -0400

    Increased the number of requests allowed on the Actions API to allow surgeing

--- public/js/client/manage.js
+++ public/js/client/manage.js
@@ -377,6 +377,7 @@ function enableReviewMode(setFromDialog) {
                 } else {
                     recentReviewDestination.unshift(reviewDestination)
                 }
+                recentReviewDestination.slice(0,5)
                 setCookie('recentReviewDestination', JSON.stringify(recentReviewDestination));
             } catch (e) {
                 console.error("Failed to save recent destinations")
@@ -694,6 +695,7 @@ function clearactionModel() {
         } else {
             recentPostDestination.unshift(postsDestination)
         }
+        recentPostDestination.slice(0,5)
         setCookie('recentPostDestination', JSON.stringify(recentPostDestination));
     } catch (e) {
         console.error("Failed to save recent destinations")
