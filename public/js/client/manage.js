a8d98fda9e7f5aa26d8d2906de3bdc67f12841f1
Yukimi Kazari <dev@9854.moe>
2022-04-26 18:12:56 -0400

    Fixes to review mode

--- public/js/client/manage.js
+++ public/js/client/manage.js
@@ -365,10 +365,11 @@ function setupReviewMode(bypass) {
         setupReviewModel.querySelector("#selectedChannel").innerText = setupReviewModel.querySelector("#destination-" + reviewDestination).getAttribute('data-ch-name')
     }
     const cleanURL = params(['nsfwEnable', 'pageinatorEnable', 'limit', 'responseType', 'key', 'blind_key', 'nsfw', 'offset', 'sort', 'search', 'color', 'date', 'displayname', 'history', 'pins', 'history_screen', 'newest', 'displaySlave', 'flagged', 'datestart', 'dateend', 'history_numdays', 'fav_numdays', 'numdays', 'ratio', 'minres', 'dark', 'filesonly', 'nocds', 'setscreen', 'screen', 'nohistory', 'reqCount'], [])
-    if (!bypass && reviewDestinationMap[`${encodeURIComponent(cleanURL)}`] !== undefined || !(reviewDestination && reviewDestination.length > 1)) {
+    if (!bypass && reviewDestinationMap[`${encodeURIComponent(cleanURL)}`] !== undefined) {
         enableReviewMode();
     } else {
         //recentDestionations
+        inReviewMode = false;
         const rdest = recentReviewDestination.filter(e => e.length > 1 && !isNaN(parseInt(e))).map(e => {
             const n = setupReviewModel.querySelector("#destination-" + e).getAttribute('data-ch-name')
             if (n) {
@@ -385,9 +386,8 @@ function setupReviewMode(bypass) {
 }
 function enableReviewMode(setFromDialog) {
     const cleanURL = params(['nsfwEnable', 'pageinatorEnable', 'limit', 'responseType', 'key', 'blind_key', 'nsfw', 'offset', 'sort', 'search', 'color', 'date', 'displayname', 'history', 'pins', 'history_screen', 'newest', 'displaySlave', 'flagged', 'datestart', 'dateend', 'history_numdays', 'fav_numdays', 'numdays', 'ratio', 'minres', 'dark', 'filesonly', 'nocds', 'setscreen', 'screen', 'nohistory', 'reqCount'], [])
-    if (reviewDestinationMap[`${encodeURIComponent(cleanURL)}`]) {
+    if (reviewDestinationMap[`${encodeURIComponent(cleanURL)}`])
         setReviewChannel(reviewDestinationMap[`${encodeURIComponent(cleanURL)}`], true);
-    }
     if (reviewDestination && reviewDestination.length > 1) {
         if (setFromDialog) {
             try {
@@ -448,6 +448,8 @@ function enableReviewMode(setFromDialog) {
         document.getElementById("reviewDestinationName").innerText = setupReviewModel.querySelector("#selectedChannel").innerText;
         document.getElementById('reviewBtns').classList.remove("hidden");
         $('#setupReviewModel').modal('hide');
+    } else {
+        setupReviewMode(true);
     }
     return false;
 }
