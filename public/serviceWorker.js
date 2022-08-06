352ad7c5d6ee21a996640eef40e2053bc059f840
Yukimi Kazari <dev@9854.moe>
2022-08-05 23:01:42 -0400

    handle`

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -2,7 +2,7 @@
 importScripts('/static/vendor/domparser_bundle.js');
 const DOMParser = jsdom.DOMParser;
 
-const cacheName = 'DEV-v20-8-PATCH90';
+const cacheName = 'DEV-v20-9';
 const cacheCDNName = 'DEV-v2-11';
 const origin = location.origin
 const offlineUrl = '/offline';
