b314bd7a18049db02e5f535a132718f1d0a6e9b8
Yukimi Kazari <dev@9854.moe>
2022-08-07 09:14:00 -0400

    kmsCac

--- public/js/client/home-lite.js
+++ public/js/client/home-lite.js
@@ -1,5 +1,26 @@
 let _Size = null;
 let _Color = null;
+let performaceMode = (getCookie("performaceMode") !== null) ? getCookie("performaceMode") === 'true' : false;
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return null;
+}
 
 $(window).on('hashchange', () => {
     if (location.hash.length > 1 && location.hash.substring(1).startsWith('/')) {
