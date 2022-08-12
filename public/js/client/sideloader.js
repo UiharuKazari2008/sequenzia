10f15fafb377ddd35a49903ab0fd9b4738cd1ec2
Yukimi Kazari <dev@9854.moe>
2022-08-12 03:13:24 -0400

    kmsCac`

--- public/js/client/sideloader.js
+++ public/js/client/sideloader.js
@@ -1846,7 +1846,7 @@ async function generateEpisodeHTML(url) {
 
                 if (episodes && episodes.episodes && episodes.show) {
                     resultRows = await Promise.all(episodes.episodes.sort(function (a, b) {
-                        return (((a.season || 0) + 1) * (a.episode || 0)) - (((b.season || 0) + 1) * (b.episode || 0));
+                        return (((b.season || 0) + 1) * (b.episode || 0)) - (((a.season || 0) + 1) * (a.episode || 0));
                     }).map(async e => {
                         return `<div class="col-12 col-sm-6 col-md-4 m-0 flex-nowrap episode-row p-1" ${(e.htmlAttributes && e.htmlAttributes.length > 0) ? e.htmlAttributes.filter(j => !j.startsWith('class=')).join(' ') : 'id="message-' + e.id + '"'}>
     <div class="episode-preview-grid">
@@ -1931,7 +1931,7 @@ async function generateEpisodeHTML(url) {
     </div>
     <div class="show-description"><span>${episodes.show.meta.description}</span></div>
 </div>
-</div><div class="show accordion accordion-flush show-background pt-4 p-sm-4" id="seasonsAccordion-${episodes.show.id}">${resultRows.join(' ')}</div>`
+</div><div class="show accordion accordion-flush show-background pt-4 p-sm-4 " id="seasonsAccordion-${episodes.show.id}"><div class="accordion-body d-flex row m-0">${resultRows.join(' ')}</div></div>`
                     document.getElementById('titleBarContents').innerHTML = `<ul class="navbar-nav text-primary text-ellipsis"><li class="nav-item text-right page-title text-primary mr-1" id="topAddressBarInfo"><div class="d-inline-flex" id="titleIcon"><i class="far mr-2 fa-tv"></i></div><div class="d-inline-flex" id="titleExtraStyleAdjustment"></div><div class="d-none d-md-inline-flex" id="titleExtra"><span class="pr-1 align-self-baseline text-uppercase">Theater</span><i class="far fa-chevron-right pr-1 align-self-baseline"></i></div><div class="d-inline" id="titleMain"><span class="align-self-baseline text-uppercase">${episodes.show.name}</span></div>`
                     registerLazyLoader();
                     registerURLHandlers();
