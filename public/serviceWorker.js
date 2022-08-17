bd51ef812747e3a502cc896404e323b75b15aaa1
Yukimi Kazari <dev@9854.moe>
2022-08-17 09:14:12 -0400

    kmsCac`

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 'use strict';
 importScripts('/static/vendor/domparser_bundle.js');
 const DOMParser = jsdom.DOMParser;
-const cacheName = 'PRODUCTION-v20-7-17-2022-BUGWATCH-P1';
+const cacheName = 'PRODUCTION-v20-7-17-2022-BUGWATCH-P2';
 const cacheCDNName = 'DEV-v2-11';
 const origin = location.origin
 const offlineUrl = '/offline';
