41d5edc539602689ed360bcd85e942a6ae1d210d
Yukimi Kazari <dev@9854.moe>
2022-09-09 02:34:13 -0400

    Added image history from home page and added another row of icons Apps can now decare a splash image, accent, and background Menu bar is slimmer

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 'use strict';
 importScripts('/static/vendor/domparser_bundle.js');
 const DOMParser = jsdom.DOMParser;
-const cacheName = 'PRODUCTION-v20-9-9-2022-BUGWATCH-P6';
+const cacheName = 'PRODUCTION-v20-9-9-2022-BUGWATCH-P7';
 const cacheCDNName = 'DEV-v2-11';
 const origin = location.origin
 const offlineUrl = '/offline';
