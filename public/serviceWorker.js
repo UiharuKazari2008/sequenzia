35624d5086d171bf4721fc7e21afdb761550cafd
Yukimi Kazari <dev@9854.moe>
2023-01-04 02:00:55 -0500

    Updated boot up display

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 'use strict';
 importScripts('/static/vendor/domparser_bundle.js');
 const DOMParser = jsdom.DOMParser;
-const cacheName = 'PRODUCTION-v20-040123'
+const cacheName = 'PRODUCTION-v20-040123-PATCH1'
 const cacheCDNName = 'DEV-v2-11';
 const origin = location.origin
 const offlineUrl = '/offline';
