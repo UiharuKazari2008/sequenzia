4147a6b408052754152a50d44cab23eefacc63e0
Yukimi Kazari <dev@9854.moe>
2023-01-09 19:44:07 -0500

    Update patch ID

--- js/redisClient.js
+++ js/redisClient.js
@@ -11,7 +11,7 @@ const global = require("../config.json");
 const Redis = require('ioredis');
 
 let redis;
-if (global.shared_caches) {
+if (global.shared_cache) {
     redis = new Redis({
         host: config.redis_host,
         port: (config.redis_port) ? parseInt(config.redis_port.toString()) : undefined,
