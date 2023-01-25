4d5bd667bb1184d324a1df264ccde8256a0276c5
Yukimi Kazari <dev@9854.moe>
2023-01-25 04:50:42 -0500

    Inital beta of cross-instance exchanges

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 'use strict';
 importScripts('/static/vendor/domparser_bundle.js');
 const DOMParser = jsdom.DOMParser;
-const cacheName = 'PRERELEASE-v20-1-170123'
+const cacheName = 'PRERELEASE-v20-1-250123'
 const cacheCDNName = 'DEV-v2-11';
 const origin = location.origin
 const offlineUrl = '/offline';
