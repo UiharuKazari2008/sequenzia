42f270e5965ac5ad71f4a8e2de0ef1efcf654591
Yukimi Kazari <dev@9854.moe>
2023-07-22 05:32:57 -0400

    kio

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 'use strict';
 importScripts('/static/vendor/domparser_bundle.js');
 const DOMParser = jsdom.DOMParser;
-const cacheName = 'PRERELEASE-v20-2-22JULY23'
+const cacheName = 'PRERELEASE-v20-2-22JULY23-PATCH2'
 const cacheCDNName = 'DEV-v2-11';
 const origin = location.origin
 const offlineUrl = '/offline';
