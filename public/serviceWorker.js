09ef2a5d94a0a432b95c193de9d79674c24db75d
Yukimi Kazari <dev@9854.moe>
2023-07-24 16:48:10 -0400

    Added pixiv and twitter RPC controls to sequenzia

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 'use strict';
 importScripts('/static/vendor/domparser_bundle.js');
 const DOMParser = jsdom.DOMParser;
-const cacheName = 'PRERELEASE-v20-2-24JULY23-PATHCH2'
+const cacheName = 'PRERELEASE-v20-2-24JULY23-PATHCH3'
 const cacheCDNName = 'DEV-v2-11';
 const origin = location.origin
 const offlineUrl = '/offline';
