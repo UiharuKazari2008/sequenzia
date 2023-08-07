79a1b0e0e803f68a01f7dec4144caf2cdfcf1cd5
Yukimi Kazari <dev@9854.moe>
2023-08-07 02:45:14 -0400

    correct dark mode edges

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 'use strict';
 importScripts('/static/vendor/domparser_bundle.js');
 const DOMParser = jsdom.DOMParser;
-const cacheName = 'PRERELEASE-v20-2-06AUG23'
+const cacheName = 'PRERELEASE-v20-2-07AUG23'
 const cacheCDNName = 'DEV-v2-11';
 const origin = location.origin
 const offlineUrl = '/offline';
