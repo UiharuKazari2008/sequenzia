b03ac79f112a8a6066d989481b935cfd4543c697
Yukimi Kazari <dev@9854.moe>
2023-09-21 22:11:59 -0400

    Test led server

--- public/js/client/ambient.js
+++ public/js/client/ambient.js
@@ -1369,9 +1369,9 @@ $(document).ready(function () {
             error: function (res) { console.error('Failed to update VFD Display') }
         });
     }
-    /*if (remoteWACCALED) {
-        sendLEDStatic("f9c8a7");
-    }*/
+    if (remoteWACCALED) {
+        sendLEDStatic("f99400");
+    }
     let _refreshURL = '/discord/refresh'
     if (config.has('key')) {
         _refreshURL += `?key=${config.getAll('key').pop()}`
