0bb07f0de156f8456de77a48b8b4a6a6e0162919
Yukimi Kazari <dev@9854.moe>
2023-09-23 00:33:28 -0400

    Test of brightness chages

--- public/js/client/ambient.js
+++ public/js/client/ambient.js
@@ -1231,7 +1231,6 @@ let allColors = [];
 function getColorData(url) {
     const image = new Image();
     image.style.opacity = "0";
-    image.style.filter = "blur(8px)"
     image.src = url;
     image.crossOrigin = "Anonymous"
     setTimeout(() => {
@@ -1241,6 +1240,7 @@ function getColorData(url) {
 async function parseCanvasToHEX(image) {
     imageCanvas.width = image.width;
     imageCanvas.height = image.height;
+    imageCtx.filter = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='a' x='0' y='0' width='10' height='1'%3E%3CfeGaussianBlur stdDeviation='15' result='b'/%3E%3CfeMorphology operator='dilate' radius='4'/%3E %3CfeMerge%3E%3CfeMergeNode/%3E%3CfeMergeNode in='b'/%3E%3C/feMerge%3E%3C/filter%3E%3C/svg%3E#a") saturate(1.2) contrast(1.2)`;
     imageCtx.drawImage(image, 0, 0);
     const center = {
         x: image.width / 2,
@@ -1263,78 +1263,7 @@ async function parseCanvasToHEX(image) {
 function rgbToHex(r, g, b) {
     return "0x" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase();
 }
-
-
-function reorderColors(colors) {
-    let reorderedColors = [];
-    const ringCount = 8;
-    const colorsPerRing = colors.length / ringCount;
-
-    // Function to convert RGB to HSL
-    function rgbToHsl(r, g, b) {
-        r /= 255;
-        g /= 255;
-        b /= 255;
-        const max = Math.max(r, g, b), min = Math.min(r, g, b);
-        let h, s, l = (max + min) / 2;
-
-        if (max === min) {
-            h = s = 0; // achromatic
-        } else {
-            const d = max - min;
-            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-            switch (max) {
-                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-                case g: h = (b - r) / d + 2; break;
-                case b: h = (r - g) / d + 4; break;
-            }
-            h /= 6;
-        }
-
-        return [h, s, l];
-    }
-
-    // Function to convert HSL to RGB
-    function hslToRgb(h, s, l) {
-        let r, g, b;
-
-        if (s === 0) {
-            r = g = b = l; // achromatic
-        } else {
-            const hue2rgb = function hue2rgb(p, q, t) {
-                if (t < 0) t += 1;
-                if (t > 1) t -= 1;
-                if (t < 1 / 6) return p + (q - p) * 6 * t;
-                if (t < 1 / 2) return q;
-                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-                return p;
-            };
-
-            const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            const p = 2 * l - q;
-            r = hue2rgb(p, q, h + 1 / 3);
-            g = hue2rgb(p, q, h);
-            b = hue2rgb(p, q, h - 1 / 3);
-        }
-
-        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-    }
-
-    for (let i = 0; i < colorsPerRing; i++) {
-        for (let j = 0; j < ringCount; j++) {
-            const colorIndex = j * colorsPerRing + i;
-            const [r, g, b] = hexToRgb(colors[colorIndex]); // Assuming you have a hexToRgb function
-            let [h, s, l] = rgbToHsl(r, g, b);
-            l = Math.max(0, l - 0.1); // Decrease lightness by 10%
-            const [newR, newG, newB] = hslToRgb(h, s, l);
-            const newHex = rgbToHex(newR, newG, newB); // Assuming you have a rgbToHex function
-            reorderedColors.push(newHex);
-        }
-    }
-
-    return reorderedColors;
-}
-/*function reorderColors(allColors) {
+function reorderColors(allColors) {
     let reordered = [];
     for (let i = 0; i < sampleCount; i++) {
         for (let j = 0; j < circleCount; j++) {
@@ -1342,7 +1271,7 @@ function reorderColors(colors) {
         }
     }
     return reordered;
-}*/
+}
 function sampleAverageColor(image, x, y) {
     const imageData = image.getImageData(x - 2, y - 2, 5, 5); // Sample a 5x5 region around (x, y)
     let totalRed = 0;
