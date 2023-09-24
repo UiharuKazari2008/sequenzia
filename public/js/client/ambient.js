f2faa794dec12cc77b5f03ed210d27e3b923b31a
Yukimi Kazari <dev@9854.moe>
2023-09-24 03:58:28 -0400

    Ω

--- public/js/client/ambient.js
+++ public/js/client/ambient.js
@@ -1339,7 +1339,7 @@ async function parseCanvasToChunithm(image) {
 function decreaseBrightness(color, percent) {
     const stage1 = tinycolor("#" + color).darken(percent);
     const _stage1 = stage1.clone();
-    const _s1hsl = _stage1.toHSL();
+    const _s1hsl = _stage1.toHsl();
     const stage2 = (_s1hsl.s > 0.6) ? stage1.saturate(percent / 8) : stage1;
     return stage2.toString().substring(1);
 
