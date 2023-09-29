0ea3fdbdd1c1490a5274b9f97f7383c32f28873d
Yukimi Kazari <dev@9854.moe>
2023-09-28 22:57:20 -0400

    Updated to ambient

--- public/js/client/ambient.js
+++ public/js/client/ambient.js
@@ -1278,7 +1278,7 @@ async function parseCanvasToChunithm(image) {
     const thresholdColor = '#00060c';
     const thresholdBrightness = 0.299 * parseInt(thresholdColor.slice(1, 3), 16) + 0.587 * parseInt(thresholdColor.slice(3, 5), 16) + 0.114 * parseInt(thresholdColor.slice(5, 7), 16);
     const maxBrightnessRatio = 0.5;
-    const aspectRatio = (16 / 9);
+    const aspectRatio = (2 / 1);
 
     let spacingX, spacingY, startX, startY;
     if (image.width / gridWidth < image.height / (gridHeight * aspectRatio)) {
