575ad9ce9d178eb931795239c3a5bba2e047b6e9
Yukimi Kazari <kazari@acr.moe>
2024-09-18 23:49:46 -0400

    UI Updates

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 'use strict';
 importScripts('/static/vendor/domparser_bundle.js');
 const DOMParser = jsdom.DOMParser;
-const cacheName = 'PRERELEASE-v22-18SEP24-PATCH1'
+const cacheName = 'PRERELEASE-v22-18SEP24-PATCH11'
 const cacheCDNName = 'DEV-v2-11';
 const origin = location.origin
 const offlineUrl = '/offline';
