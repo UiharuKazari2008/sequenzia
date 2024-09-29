8ae8ae021ec32697c4bfb7354e77379b5ff5a88c
UiharuKazari2008 <nomail@internet.net>
2024-09-29 06:47:25 -0400

    UI Updates

--- js/generalActions.js
+++ js/generalActions.js
@@ -266,14 +266,9 @@ module.exports = async (req, res, next) => {
                             let itemsToAdd = req.body.messagelist;
                             const existingItems = found.map(e => e.eid);
                             if (found.length !== 0)
-                                itemsToAdd = itemsToAdd.filter(e => existingItems.indexOf(e) === -1).map(e => {
-                                    return {
-                                        eid: e,
-                                        aid: req.body.albumid
-                                    }
-                                });
+                                itemsToAdd = itemsToAdd.filter(e => existingItems.indexOf(e) === -1).map(e => `(${e}, ${req.body.albumid})`);
                             if (itemsToAdd.length > 0) {
-                                sqlSafe(`INSERT INTO sequenzia_album_items (eid, aid) VALUES (?, ?)`, itemsToAdd, (err, result) => {
+                                sqlSafe(`INSERT INTO sequenzia_album_items (eid, aid) VALUES ${itemsToAdd.join(', ')}`, (err, result) => {
                                     if (err) {
                                         printLine("ActionParser", `Unable to build add ${itemsToAdd.length} items to ${req.body.data} for ${thisUser.discord.user.id}: ${err.sqlMessage}`, 'error', err)
                                         res.status(500).send('Database Error');
