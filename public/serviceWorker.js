0882834b2f19b5db82bf4b985584a4bcada4831f
Yukimi Kazari <kazari@acr.moe>
2024-10-14 18:21:56 -0400

    `Updated for key bindings`

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 'use strict';
 importScripts('/static/vendor/domparser_bundle.js');
 const DOMParser = jsdom.DOMParser;
-const cacheName = 'PRERELEASE-v22-14OCT24'
+const cacheName = 'PRERELEASE-v22-14OCT24-P1'
 const cacheCDNName = 'DEV-v2-11';
 const origin = location.origin
 const offlineUrl = '/offline';
